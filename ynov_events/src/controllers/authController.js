e131ab9a71aa3ca716a87af6d72bd8e7688968fa
SatanVI <Ahmedbangoura852@gmail.com>
2026-04-18 17:59:22 +0100

    fix: resolve Prisma ReferenceError in authController and cleanup duplicates

--- ynov_events/src/controllers/authController.js
+++ ynov_events/src/controllers/authController.js
@@ -1,3 +1,7 @@
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const { loginSchema } = require('../utils/validation');
 
 exports.login = async (req, res) => {
